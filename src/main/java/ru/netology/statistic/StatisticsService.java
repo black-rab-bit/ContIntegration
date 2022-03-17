8de0bc69670c7b3b3da0d49985b09e1bd966cef6
TesterSinet <tanya@sinet.team>
2022-03-18 00:25:30 +0900

    удален MaxIndex

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,10 +1,7 @@
 package ru.netology.statistic;
 
-import org.jetbrains.annotations.NotNull;
-
 public class StatisticsService {
-    public long findMax(long @NotNull [] incomes) {
-        long currentMaxIndex = 0;
+    public long findMax(long[] incomes) {
         long currentMax = incomes[0];
         for (long income : incomes)
             if (currentMax < income)
