35350323056ca6f0f211e9574aab9739dfe8e4ef
TesterSinet <tanya@sinet.team>
2022-03-17 23:15:19 +0900

    try to 100

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,8 +10,20 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, -2, 18, 14, 0, 32, 7, 4, 10, 6, 3};
+        long expected = 32;
 
         long actual = service.findMax(incomesInBillions);
 
