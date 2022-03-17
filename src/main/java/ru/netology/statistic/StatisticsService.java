dc08e0c71f74b4356b4fcb8e9afc9a96253dfcef
TesterSinet <tanya@sinet.team>
2022-03-18 00:09:44 +0900

    next try to 100

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,12 +1,6 @@
 package ru.netology.statistic;
 
 public class StatisticsService {
-    /**
-     * Calculate index of max income
-     *
-     * @param incomes - array of incomes
-     * @return - index of first max value
-     */
     public long findMax(long[] incomes) {
         long currentMaxIndex = 0;
         long currentMax = incomes[0];
